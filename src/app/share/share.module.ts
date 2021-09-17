310dfbdd966f08c4055850968ab7040c52acb562
JC <ja166667167@gmail.com>
2021-09-17 16:56:41 +0800

    share module and Material Module created (#26)

---
+++ src/app/share/share.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { MaterialModule } from './material/material.module';
+
+@NgModule({
+  declarations: [],
+  imports: [MaterialModule],
+  exports: [MaterialModule],
+})
+export class ShareModule {}
